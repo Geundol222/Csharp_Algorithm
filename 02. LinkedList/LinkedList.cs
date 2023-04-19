b4ad7f9264d150311c7946d4ce30bb347d953d2d
현석 이 <geundol222@gmail.com>
2023-04-19 18:01:43 +0900

    feat : LinkedList AddFirst, AddLast 구현
    LinkedList.AddFirst, 및 AddLast 기능 구현

--- 02. LinkedList/LinkedList.cs
+++ 02. LinkedList/LinkedList.cs
@@ -4,9 +4,103 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _02._LinkedList
+namespace DataStructure
 {
-    internal class LinkedList
+    public class LinkedListNode<T>
     {
+        internal LinkedList<T> list;
+        internal LinkedListNode<T> prev;
+        internal LinkedListNode<T> next;
+        private T item;
+
+        public LinkedListNode(T value)
+        {
+            this.list = null;
+            this.prev = null;
+            this.next = null;
+            this.item = value;
+        }
+
+        public LinkedListNode(LinkedList<T> list, T value)
+        {
+            this.list = list;
+            this.prev = null;
+            this.next = null;
+            this.item = value;
+        }
+
+        public LinkedListNode(LinkedList<T> list, LinkedListNode<T> prev, LinkedListNode<T> next, T value)
+        {
+            this.list = list;
+            this.prev = prev;
+            this.next = next;
+            this.item = value;
+        }
+
+        public LinkedList<T> List { get { return list; } }
+        public LinkedListNode<T> Prev { get { return prev; } }
+        public LinkedListNode<T> Next { get { return next; } }
+        public T Value { get { return item; } set { item = value; } }
+    }
+
+    public class LinkedList<T>
+    {
+        private LinkedListNode<T> head;
+        private LinkedListNode<T> tail;
+        private int count;
+
+        public LinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+            this.count = 0;
+        }
+
+        public LinkedListNode<T> First { get { return head; } }
+        public LinkedListNode<T> Last { get { return tail; } }
+        public int Count { get { return count; } }
+
+        public LinkedListNode<T> AddFirst(T value)
+        {
+            // 1. 새로운 노드 생성
+            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+
+            // 2. 연결구조 바꾸기
+            if (head != null)
+            {
+                // 2-1. Head 노드가 있을 경우
+                newNode.next = head;
+                head.prev = newNode;
+                head = newNode; // 3. 새로운 노드를 head노드로 지정
+            }
+            else    // 2-2. head 노드가 없을 경우
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            count++;
+
+            return newNode;
+        }
+
+        public LinkedListNode<T> AddLast(T value)
+        {
+            LinkedListNode<T> lastNode = new LinkedListNode<T>(this, value);
+
+            if (tail != null)
+            {
+                lastNode.prev = tail;
+                tail.next = lastNode;
+                tail = lastNode;
+            }
+            else
+            {
+                head = lastNode;
+                tail = lastNode;
+            }
+            count++;
+
+            return lastNode;
+        }
     }
 }
--- 02. LinkedList/Program.cs
+++ 02. LinkedList/Program.cs
@@ -12,6 +12,12 @@
 		 * 하지만, 연속적인 데이터가 아니므로 중간의 것을 지우거나 중간에 삽입하는데에 제약이 없으므로 매우 효율적이다.
 		 ******************************************************/
 
+        /* <연결리스트의 종류>
+         * 1. 단방향 LinkedList : 참조용 데이터가 하나 적으므로 데이터를 아낄 수 있으며, 역방향이 굳이 필요없다면 사용
+         * 2. 양방향 LinkedList : 이전 데이터도 참조하고 있으므로 여러 작업이 가능해지지만, 양방향을 참조하므로 데이터 양이 많아질 수록 비효율 적이다.
+         * 3. 원형(환형) LinkedList : C#에서 쓰이는 LinkedList 맨 마지막데이터는 맨 처음 데이터를 참조하고 맨 처음 데이터의 Prev는 맨 마지막 데이터를 참조한다.
+         */
+
         // <링크드리스트 사용>
         void LinkedList()
         {
@@ -47,30 +53,13 @@
 
         static void Main(string[] args)
         {
-            LinkedList<string> linkedList = new LinkedList<string>();
+            DataStructure.LinkedList<int> linkedList = new DataStructure.LinkedList<int>();
 
-            // 링크드리스트 요소 삽입
-            linkedList.AddFirst("0번 앞데이터");
-            linkedList.AddFirst("1번 앞데이터");
-            linkedList.AddLast("0번 뒤데이터");
-            linkedList.AddLast("1번 뒤데이터");
-
-            // 링크드리스트 요소 삭제
-            linkedList.Remove("1번 앞데이터");
-
-            // 링크드리스트 요소 탐색
-            LinkedListNode<string> findNode = linkedList.Find("0번 뒤데이터");
-
-            // 링크드리스트 노드를 통한 노드 참조
-            LinkedListNode<string> prevNode = findNode.Previous;
-            LinkedListNode<string> nextNode = findNode.Next;
-
-            // 링크드리스트 노드를 통한 노드 삽입
-            linkedList.AddBefore(findNode, "찾은노드 앞데이터");
-            linkedList.AddAfter(findNode, "찾은노드 뒤데이터");
-
-            // 링크드리스트 노드를 통한 삭제
-            linkedList.Remove(findNode);
+            linkedList.AddLast(0);
+            linkedList.AddLast(1);
+            linkedList.AddLast(2);
+            linkedList.AddLast(3);
+            linkedList.AddLast(4);
         }
     }
 }
