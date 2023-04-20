b6b4af896de155918e638e67862629b33148d41a
현석 이 <geundol222@gmail.com>
2023-04-20 16:16:29 +0900

    docs : 반복기 주석 추가

--- 03. Iterator/Program.cs
+++ 03. Iterator/Program.cs
@@ -11,15 +11,30 @@
 
         static void Main(string[] args)
         {
+            // 대부분의 자료구조가 반복기를 지원함
+            // 반복기를 이용한 기능을 구현할 경우, 그 기능은 대부분 자료구조를 호환할 수 있음
             List<int> list = new List<int>();
             LinkedList<int> linkedList = new LinkedList<int>();
-
             Stack<int> stack = new Stack<int>();
             Queue<int> queue = new Queue<int>();
             SortedList<int, int> sList = new SortedList<int, int>();
             SortedSet<int> set = new SortedSet<int>();
             SortedDictionary<int, int> map = new SortedDictionary<int, int>();
             Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            // 반복기를 이용한 순회
+            // foreach 반복문은 데이터집합의 반복기를 통해서 단계별로 반복
+            // 즉, 반복기가 있다면 foreach 반복문으로 순회 가능
+            foreach (int i in list) { }
+            foreach (int i in linkedList) { }
+            foreach (int i in stack) { }
+            foreach (int i in queue) { }
+            foreach (int i in set) { }
+            foreach (KeyValuePair<int, int> i in sList) { }
+            foreach (KeyValuePair<int, int> i in map) { }
+            foreach (KeyValuePair<int, int> i in dic) { }
+            foreach (int i in IterFunc()) { }
+
 
             for (int i = 1; i <= 5; i++)
             {
