2af66e0de55e635ab7f40997920f9b55b0629348
현석 이 <geundol222@gmail.com>
2023-04-20 16:06:42 +0900

    feat : 반복자패턴을 사용하지 않을경우

--- 03. Iterator/Program.cs
+++ 03. Iterator/Program.cs
@@ -11,7 +11,32 @@
 
         static void Main(string[] args)
         {
-            
+            List<int> list = new List<int>();
+            LinkedList<int> linkedList = new LinkedList<int>();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                list.Add(i);
+                linkedList.AddLast(i);
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            // LinkedList는 인덱스의 개념이 없으므로 다음과 같이 작성하게 되면 오류 발생
+            //for (int i = 0; i < linkedList.Count; i++)
+            //{
+                //Console.WriteLine(linkedList[i]);
+            //}
+
+            LinkedListNode<int> node = linkedList.First;
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
         }
     }
 }
