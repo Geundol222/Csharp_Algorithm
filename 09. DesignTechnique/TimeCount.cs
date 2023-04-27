db1c88fd09445e8f8b419b773ec1af6d30698e8e
현석 이 <geundol222@gmail.com>
2023-04-27 19:16:48 +0900

    feat : 탐욕알고리즘으로 대기열 문제 구현

---
+++ 09. DesignTechnique/TimeCount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._DesignTechnique
+{
+    public class TimeCount
+    {
+        public static int CountTime(params int[] task)
+        {
+            Array.Sort(task);
+
+            int result = 0;
+
+            for (int i = 0; i < task.Length; i++)
+                result += task[i];
+
+            return result;
+        }
+    }
+}
