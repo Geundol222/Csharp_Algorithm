7351fb38dff0dc72c6e109343b15e92fc14ff9f4
Geundol222 <geundol222@gmail.com>
2023-05-01 19:49:52 +0900

    feat : 순차 탐색과 이진 탐색

---
+++ 11. Searching/Searching.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._Searching
+{
+    public class Searching
+    {
+        // <순차 탐색>
+        public static int SequentialSearch<T>(in IList<T> list, in T item) where T : IEquatable<T>
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (item.Equals(list[i]))
+                    return i;
+            }
+            return -1;
+        }
+
+        // <이진 탐색> : 정렬된 자료구조의 탐색, 이진탐색을 진행하기 전에 자료구조를 정렬하고 사용하여야한다.
+        public static int BinarySearch<T>(in IList<T> list, in T item) where T : IComparable<T>
+        {
+            int low = 0;
+            int high = list.Count - 1;
+
+            while (low <= high)
+            {
+                int middle = (low + high) >> 1;     // == (low + high) / 2;
+                int compare = list[middle].CompareTo(item);
+
+                if (compare < 0)
+                    low = middle + 1;
+                else if (compare > 0)
+                    high = middle - 1;
+                else
+                    return middle;
+            }
+            return -1;
+        }
+    }
+}
