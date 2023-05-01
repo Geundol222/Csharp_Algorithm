a4cefc5d72e6e84699d964af086df58bcef9040e
Geundol222 <geundol222@gmail.com>
2023-05-01 21:32:15 +0900

    feat : DFS BFS 구현

--- 11. Searching/Searching.cs
+++ 11. Searching/Searching.cs
@@ -39,5 +39,72 @@ namespace _11._Searching
             }
             return -1;
         }
+
+        // <깊이 우선 탐색 (Depth First Search)>
+        // 그래프의 분기를 만났을 때 최대한 깊이 내려간 뒤,
+        // 더이상 깊이 갈 곳이 없을 경우 다음 분기를 탐색
+        // 백트래킹 알고리즘 (분할정복)
+        // stack 구조에 의한 구현
+        public static void DFS(bool[,] graph, int start, out bool[] visited, out int[] parents)
+        {
+            visited = new bool[graph.GetLength(0)];
+            parents = new int[graph.GetLength(0)];
+
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                visited[i] = false;
+                parents[i] = -1;
+            }
+            SearchNode(graph, start, visited, parents);
+        }
+
+        private static void SearchNode(bool[,] graph, int start, bool[] visited, int[] parents)
+        {
+            visited[start] = true;
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                if (graph[start, i] &&          // 연결되어 있는 정점이며,
+                    !visited[i])                // 방문한 적 없는 정점인 경우
+                {
+                    parents[i] = start;
+                    SearchNode(graph, i, visited, parents);
+                }
+            }
+        }
+
+        // <너비 우선 탐색 (Breadth Frist Search)>
+        // 그래프의 분기를 만났을 때 모든 분기를 하나씩 저장하고,
+        // 저장한 한번씩 거치면서 탐색
+        // Queue구조에 의한 구현
+        public static void BFS(bool[,] graph, int start, out bool[] visited, out int[] parents)
+        {
+            visited = new bool[graph.GetLength(0)];
+            parents = new int[graph.GetLength(0)];
+
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                visited[i] = false;
+                parents[i] = -1;
+            }
+
+            Queue<int> bfsQueue = new Queue<int>();
+
+            bfsQueue.Enqueue(start);
+            while (bfsQueue.Count > 0)
+            {
+                int next = bfsQueue.Dequeue();
+                visited[next] = true;
+
+                for (int i = 0; i < graph.GetLength(0); i++)
+                {
+                    if (graph[start, i] &&          // 연결되어 있는 정점이며,
+                        !visited[i])                // 방문한 적 없는 정점인 경우
+                    {
+                        parents[i] = start;
+                        bfsQueue.Enqueue(i);
+                    }
+                }
+            }
+        }
     }
 }
