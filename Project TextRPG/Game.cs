02346072a63b7e490dc824592df7d598e8bac8a3
Geundol222 <geundol222@gmail.com>
2023-05-04 17:07:22 +0900

    feat : Game

--- Project TextRPG/Game.cs
+++ Project TextRPG/Game.cs
@@ -8,5 +8,44 @@ namespace Project_TextRPG
 {
     internal class Game
     {
+        private bool isRunning = true;
+
+        public void Run()
+        {
+            // 초기화
+            Init();
+
+            // 게임 루프
+            while (isRunning)
+            {
+                // 랜더링(Render)
+                Render();
+                // 갱신(Update)
+                Update();
+            }
+
+            // 마무리
+            Release();
+        }
+
+        private void Init()
+        {
+
+        }
+
+        private void Render()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void Release()
+        {
+
+        }
     }
 }
